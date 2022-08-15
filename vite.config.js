415495e73a4a38c755569c488cdbeed2d43c9835
Zehra Sena Akgül <74866817+zehrasenakgul@users.noreply.github.com>
2022-08-15 18:31:32 +0300

    vite.js remove

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import removeConsole from "vite-plugin-remove-console";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+    plugins: [vue(), removeConsole()],
 });
